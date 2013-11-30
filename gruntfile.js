a902cef0aa1a927486f9ef9df11eefab75f865a3
Kurtis Kemple <kurtiskemple@gmail.com>
2013-11-29 22:13:20 -0500

    added .jshintrc

--- gruntfile.js
+++ gruntfile.js
@@ -3,7 +3,11 @@ module.exports = function(grunt) {
     grunt.initConfig({
         pkg: grunt.file.readJSON('package.json'),
         jshint: {
-            all: ['gruntfile.js', 'public/js/**/*.js']
+            options: {
+                jshintrc: '.jshintrc'
+            },
+            beforeconcat: ['gruntfile.js', 'public/js/**/*.js'],
+            afterconcat: ['public/dist/*.js']
         },
         jasmine: {
             src: ['scripts/js/**/*.js'],
@@ -37,35 +41,43 @@ module.exports = function(grunt) {
                 }
             },
             blade: {
-                files: ['app/views/**/*.blade.php'],
+                files: ['app/views/**/*.blade.php', '!app/views/emails/**/*.blade.php'],
                 livereload: true
             }
         },
         less: {
           development: {
-            files: {
-              
-            }
+            files: [{
+                  expand: true,
+                  cwd: 'styles/less',
+                  src: 'app.less',
+                  dest: 'dist/css/app.css'
+            }]
           },
           production: {
             options: {
               cleancss: true
             },
-            files: {
-              
-            }
+            files: [{
+                  expand: true,
+                  cwd: 'styles/less',
+                  src: 'app.less',
+                  dest: 'dist/css/app.css'
+            }]
           }
         },
     });
 
     grunt.loadNpmTasks('grunt-contrib-jshint');
     grunt.loadNpmTasks('grunt-contrib-uglify');
-    //grunt.loadNpmTasks('grunt-contrib-concat');
-    grunt.loadNpmTasks('grunt-contrib-jasmine');
-    //grunt.loadNpmTasks('grunt-contrib-copy');
-    //grunt.loadNpmTasks('grunt-contrib-clean');
     grunt.loadNpmTasks('grunt-contrib-watch');
     grunt.loadNpmTasks('grunt-contrib-less');
+    grunt.loadNpmTasks('grunt-contrib-jasmine');
+    //grunt.loadNpmTasks('grunt-contrib-concat');
+    //grunt.loadNpmTasks('grunt-contrib-copy');
+    //grunt.loadNpmTasks('grunt-contrib-clean');
 
-    
+    grunt.registerTask('default', ['less:development', 'jshint:beforeconcat', 'jasmine', 'phpunit']);
+    grunt.registerTask('test', ['jshint:beforeconcat', 'jasmine', 'phpunit']);
+    grunt.registerTask('production', ['less:production', 'jshint:beforeconcat', 'jasmine', 'uglify', 'jshint:afterconcat', 'phpunit'])
 };
